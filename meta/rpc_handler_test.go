05bafc5e8c241f9e7f77473b86b023e302db357f
JiaShuo <js982986555@live.com>
2020-12-28 15:19:23 +0800

    update test

--- meta/rpc_handler_test.go
+++ meta/rpc_handler_test.go
@@ -2,6 +2,7 @@ package meta
 
 import (
 	"context"
+	"github.com/XiaoMi/pegasus-go-client/idl/base"
 	"github.com/XiaoMi/pegasus-go-client/idl/replication"
 	"github.com/XiaoMi/pegasus-go-client/idl/rrdb"
 	"github.com/stretchr/testify/assert"
@@ -21,4 +22,9 @@ func TestQueryConfig(t *testing.T) {
 	args.Query.AppName = "temp"
 	resp := globalClusterManager.queryConfig(ctx, args).(*rrdb.MetaQueryCfgResult)
 	assert.Equal(t, 8, len(resp.Success.Partitions))
+	assert.Equal(t, &base.ErrorCode{Errno: base.ERR_OK.String()}, resp.Success.Err)
+
+	args.Query.AppName = "notExist"
+	resp = globalClusterManager.queryConfig(ctx, args).(*rrdb.MetaQueryCfgResult)
+	assert.Equal(t, &base.ErrorCode{Errno: base.ERR_OBJECT_NOT_FOUND.String()}, resp.Success.Err)
 }
